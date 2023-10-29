25568151d28ff04ef111676e4f94061eaf2e5314
Marcin Weglowski <marcin.weglowski1@gmail.com>
2023-10-29 17:10:23 +0100

    design

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,47 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import Goal from './components/Goal.tsx'
-import Header from './components/Header.tsx'
-import goalsImg from './assets/react.svg'
+import Goal from "./components/Goal.tsx";
+import Header from "./components/Header.tsx";
+import goalsImg from "./assets/react.svg";
 
 type Goal = {
   title: string;
   description: string;
   id: number;
-}
+};
 
 const App = () => {
   const [goals, setGoals] = useState<Array<Goal>>([]);
 
   const addGoalHandler = (newGoal: Goal) => {
     setGoals((prevGoals: Array<Goal>) => {
-      return { ...prevGoals, newGoal }
-    })
-  }
+      return { ...prevGoals, newGoal };
+    });
+  };
 
   return (
-    <>
-      <div className='border-x-4 w-full max-w-[600px] m-auto mt-6 hover:border-cyan-400 duration-300 flex flex-col'>
+    <div className="flex justify-center">
+      <div className="gradient"></div>
+      <div className="w-full max-w-[600px] m-auto mt-6 hover:border-cyan-400 duration-300 flex flex-col">
         <Header image={{ src: goalsImg, alt: "List of goals" }}>
-          <h1 className='font-bold text-3xl text-center'>GOALS</h1>
+          <h1 className="font-bold text-3xl text-center custom-font">Goals</h1>
         </Header>
 
-        <button onClick={addGoalHandler} className=' text-cyan-400 font-bold py-3 px-16 mx-auto mt-11 border-2 border-cyan-400 hover:bg-cyan-400 hover:text-black duration-300 '>NEW</button>
+        {/* <button onClick={addGoalHandler} className=' text-cyan-400 font-bold py-3 px-16 mx-auto mt-11 border-2 border-cyan-400 hover:bg-cyan-400 hover:text-black duration-300 '>NEW</button> */}
 
-        <div className='p-4 flex flex-col gap-2'>
-          <Goal title="TypeScript" description="React + TypeScript Maximilian Schwarzmueller" />
-          <Goal title="Express" description="Learn REST API, creating server, manage requests" />
+        <div className="p-4 flex flex-col gap-2">
+          <Goal
+            title="TypeScript"
+            description="React + TypeScript Maximilian Schwarzmueller"
+          />
+          <Goal
+            title="Express"
+            description="Learn REST API, creating server, manage requests"
+          />
         </div>
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
--- src/components/Goal.tsx
+++ src/components/Goal.tsx
@@ -13,14 +13,14 @@ interface GoalProps {
 // const Goal: FC<GoalProps> = ({ title, description }) => {
 const Goal = ({ title, description }: GoalProps) => {
   return (
-    <div className='border-2 flex justify-between p-2 items-center hover:border-cyan-400 duration-300'>
+    <div className='border-2 border-transparent flex justify-between p-2 items-center hover:border-cyan-700 duration-300 rounded-md bg-[#00121B]/50 backdrop-blue-lg'>
       <div className='flex flex-col text-left'>
-        <div className='text-xl fond-bold'>{title}</div>
-        <div>{description}</div>
+        <div className='text-xl fond-bold text-slate-300'>{title}</div>
+        <div className="text-slate-500">{description}</div>
       </div>
       <div className='flex gap-2'>
-        <button className='hover:text-cyan-400 duration-300'>Finished</button>
-        <button className='hover:text-red-500 duration-300'>Delete</button>
+        <button className='hover:text-cyan-400 duration-300 text-slate-300'>Finished</button>
+        <button className='hover:text-red-500 duration-300 text-slate-300'>Delete</button>
       </div>
     </div>
   )
